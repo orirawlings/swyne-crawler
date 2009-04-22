f831f6962b136095efe2f5b5f45eaf63b51a14c4
Ori Rawlings <orirawlings@gmail.com>
2009-04-21 22:44:26 -0500

    Added waiting for response from indexer before closing connection.

--- src/edu/iit/swyne/crawler/SwyneIndexer.java
+++ src/edu/iit/swyne/crawler/SwyneIndexer.java
@@ -1,6 +1,8 @@
 package edu.iit.swyne.crawler;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -31,10 +33,16 @@ public class SwyneIndexer extends Indexer {
 		try {
 			Socket swyne = new Socket(this.host, this.port);
 			PrintWriter out = new PrintWriter(swyne.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(swyne.getInputStream()));
 			
 			out.println(message);
 			
+			String line;
+			while ((line = in.readLine()) != null)
+				System.out.println("Response from Swyne Indexer: " + line);
+			
 			out.close();
+			in.close();
 			swyne.close();
 		} catch (UnknownHostException e) {
 			System.err.println("ERROR: "+e.getMessage());
